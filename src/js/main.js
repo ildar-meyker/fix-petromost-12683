77685fff8cbd1d06e350af4884feb022ac86333d
Ildar Meyker <ildar.meyker@gmail.com>
2021-12-01 20:53:38 +0300

    Add progress.

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,89 @@
-import "./modules/globals";
-import "./modules/Notify";
+// import "./modules/globals";
+// import "./modules/Notify";
+
+const PopupBasket = {
+	_$panel: $(),
+
+	setHeight() {
+		const $panel = this._$panel;
+
+		const windowHeight = $(window).height();
+		const panelOffsetTop = $panel.offset().top - $(window).scrollTop();
+		const panelBottomSpace = 10;
+		const newHeight = windowHeight - panelOffsetTop - panelBottomSpace;
+
+		$panel
+			.css({
+				"max-height": "none",
+				"min-height": "300px",
+			})
+			.outerHeight(newHeight);
+	},
+
+	openPanel() {
+		this._$panel.addClass("active");
+		SliderBasket.refresh();
+		this.setHeight();
+	},
+
+	closePanel() {
+		this._$panel.removeClass("active");
+	},
+
+	_handleOpenButton(e) {
+		e.preventDefault();
+
+		if ($(e.target).closest(".js-popup-basket-panel").length) {
+			return;
+		}
+
+		if (this._$panel.hasClass("active")) {
+			this.closePanel();
+		} else {
+			this.openPanel();
+		}
+	},
+
+	_handleOutsideClick(e) {
+		if ($(e.target).closest(".js-popup-basket-open").length === 0) {
+			this.closePanel();
+		}
+	},
+
+	init() {
+		this._$panel = $("#popup-basket");
+
+		$(document).on(
+			"click",
+			".js-popup-basket-open",
+			this._handleOpenButton.bind(this)
+		);
+
+		$(document).on("click", this._handleOutsideClick.bind(this));
+	},
+};
+
+const SliderBasket = {
+	_$root: $(),
+
+	refresh() {
+		this._$root.find(".slider-basket__list").slick("setPosition");
+	},
+
+	init() {
+		this._$root = $("#slider-basket");
+
+		this._$root.find(".slider-basket__list").slick({
+			infinite: false,
+			slidesToShow: 2,
+			slidesToScroll: 1,
+			prevArrow: this._$root.find(".slider-basket__prev"),
+			nextArrow: this._$root.find(".slider-basket__next"),
+		});
+	},
+};
 
 $(function () {
-	console.log("Hello, world!");
+	SliderBasket.init();
+	PopupBasket.init();
 });
